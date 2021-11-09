3cd256e8fb2fc2e7cde17ea747678f73dcf37cb2
chetTest <alexu-che@yandex.ru>
2021-11-09 12:22:41 +0300

    Встреча  23.10.21

---
+++ 18/23102021_18.py
@@ -0,0 +1,13 @@
+f = open('18-18.csv')
+data = f.readlines()
+summ = float(data[0])
+maxSum = float(data[0])
+for i in range(1, len(data)):
+    if float(data[i]) > float(data[i-1]):
+        summ += float(data[i])
+    else:
+        summ = float(data[i])
+    if summ > maxSum:
+        maxSum = summ
+print(maxSum)
+
