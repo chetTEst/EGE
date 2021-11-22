59c22243ac0375725b24c7f7e088dfb39fcfc126
chetTest <alexu-che@yandex.ru>
2021-11-22 13:54:56 +0300

    Пример решения задания 5

--- 2/taskExample.py
+++ 2/taskExample.py
@@ -2,5 +2,5 @@ print("(x /\ y) -> (!x /\ !z")
 for x in range (2):
   for y in range (2):
     for z in range(2):
-        if (x * y) <= (not(x) * (not(z)))  :
+        if ((x * y) <= (not(x) * (not(z)))):
           print(x,y,z)
